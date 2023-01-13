5e4a57de8e0317b907105e39db16a2770a1ccf67
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-13 10:55:21 +0100

    Affichage avec querySelectorAll

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@
 // ### 1. Récupère avec getElementsByClassName "redPurple" affiche et observe
 console.log(document.getElementsByClassName('redPurple'));
 // ### 2. Fais la meme chose qu'a l'exercice 1 mais avec un querySelectorAll, observe la différence
+console.log(document.querySelectorAll('.redPurple'));
 // ### 3. En utilisant querySelectorAll trouve un moyen de ne récupérer que les h1 ayant la class "redPurple"
 // ### 4. Trouve le moyen de récupèrer en un seul querySelectorAll tous les éléments li, les éléments p et les éléments span
 // ### 5. Trouve le moyen de récupèrer en un seul querySelectorAll tous les éléments li ayant la class important et les éléments p 
