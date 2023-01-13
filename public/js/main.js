937bd0c8e3366af63bc6a0aaebda9cb36bcde031
Bachir Boulaich <bachir.boulaich@outlook.com>
2023-01-13 10:57:50 +0100

    Affichage de tous les li, p et span

--- public/js/main.js
+++ public/js/main.js
@@ -10,5 +10,6 @@ console.log(document.querySelectorAll('.redPurple'));
 // ### 3. En utilisant querySelectorAll trouve un moyen de ne récupérer que les h1 ayant la class "redPurple"
 console.log(document.querySelectorAll('h1.redPurple'));
 // ### 4. Trouve le moyen de récupèrer en un seul querySelectorAll tous les éléments li, les éléments p et les éléments span
+console.log(document.querySelectorAll('li, p, span'));
 // ### 5. Trouve le moyen de récupèrer en un seul querySelectorAll tous les éléments li ayant la class important et les éléments p 
 // ### 6. Récupère en un seul querySelectorAll uniquement les h1 et les span ayant la class "redPurple"
